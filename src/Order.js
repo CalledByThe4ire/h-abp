4ceed5812fd895c6a0cd5680785c531ac4e917bb
Andrey Karamnoff <karamnoff@gmail.com>
2019-04-21 20:13:16 +0300

    add solution for 06-state_machine_pattern

--- src/Order.js
+++ src/Order.js
@@ -13,12 +13,20 @@ StateMachine.factory(Order, {
   init: 'init',
   transitions: [
     // BEGIN (write your solution here)
-
+    { name: 'accept', from: 'init', to: 'pending' },
+    { name: 'ship', from: 'pending', to: 'shipped' },
+    { name: 'complete', from: 'shipped', to: 'completed' },
+    { name: 'cancel', from: ['init', 'pending'], to: 'canceled' },
+    { name: 'refund', from: ['shipped', 'completed'], to: 'refunded' },
     // END
   ],
   methods: {
     // BEGIN (write your solution here)
-
+    onEnterState({ transition }) {
+      if (transition !== 'none' && transition !== 'init') {
+        this.history.push({ state: this.state, createdAt: new Date() });
+      }
+    },
     // END
   },
 });
--- src/solution.js
+++ src/solution.js
@@ -3,5 +3,11 @@ import Order from './Order';
 export const init = items => new Order(items);
 
 // BEGIN (write your solution here)
-
+export const tryCancel = (order) => {
+  if (order.is('init') || order.is('pending')) {
+    order.cancel();
+    return order.state;
+  }
+  return order.state;
+};
 // END
